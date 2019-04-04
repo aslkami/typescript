e17f42be68b6d4c1e3b06199616d388b5db3cdc5
zhuangan <zhuang.an@vpclub.cn>
2019-04-04 18:38:19 +0800

    company test

---
+++ js/hello.js
@@ -0,0 +1,7 @@
+"use strict";
+function sayHello(person) {
+    return 'Hello, ' + person;
+}
+var user = 'Tom';
+console.log(sayHello(user));
+var createdByNewBoolean = Boolean(1);
---
+++ ts/hello.ts
@@ -0,0 +1,8 @@
+function sayHello(person: string) {
+  return 'Hello, ' + person;
+}
+
+let user = 'Tom';
+console.log(sayHello(user));
+
+let createdByNewBoolean: boolean = Boolean(1);
